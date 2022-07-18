422f813a9426a968828b250bd0184369075309da
Mateus Daniel <mateus.silva@tritone.com.br>
2022-07-18 17:43:23 -0300

    feat: finaliza a seção de setar a quantidade de equipes.

--- src/js/main.js
+++ src/js/main.js
@@ -9,10 +9,18 @@
     let sports_input = document.querySelectorAll('.sec-seletor__custom_radio input');
     let sports_btProx = document.querySelector('.sec-seletor__custom_radio + .bt');
     
+    let teams_input = document.querySelector('.sec-qtd-equipes__input');
+    let teams_btProx = document.querySelector('.bt.sec-qtd-equipes__bt.bt-next');
+
+    let sports_selected = '';
+    let teams_qtd = 0;
+    
     sports_input.forEach((el) => {
         el.addEventListener('change', () => {
             if(el.checked) {
                 sports_btProx.removeAttribute('disabled');
+
+                sports_selected = el.value;
             }
         });
     });
@@ -28,6 +36,20 @@
             sections[i].style.marginLeft = '';
         });
     });
+    
+    teams_input.addEventListener('input', () => {
+
+        if(teams_input.value != '' && teams_input.value >= 2 && teams_input.value <= 20) {
+            teams_btProx.removeAttribute('disabled');
+        } else {
+            teams_btProx.setAttribute('disabled', '');
+        }
+    });
+
+    teams_btProx.addEventListener('click', () => {
+        teams_qtd = teams_input.value;
+    })
+
 
 
     
